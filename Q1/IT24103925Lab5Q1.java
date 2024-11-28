e20438a20dd823eccc6e8532057d6a84c842bc86
IT24103925 <it24103925@my.sliit.lk>
2024-11-28 11:24:34 +0530

    Add files via upload

--- Q1/IT24103925Lab5Q1.java
+++ Q1/IT24103925Lab5Q1.java
@@ -4,7 +4,6 @@ public class IT24103925Lab5Q1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
         System.out.print("Enter the first integer: ");
         int num1 = scanner.nextInt();
         System.out.print("Enter the second integer: ");
